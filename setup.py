5d2f452035000a521d20474d925d5a8bbd8dd9cf
José Arturo García <jose.arturo.garcia@gmail.com>
2026-07-10 21:27:46 -0400

    Fix package build isolation

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import find_packages, setup
 
-from mseller_ecf import __version__ as version
-
 with open("requirements.txt") as f:
     install_requires = f.read().strip().splitlines()
 
 setup(
     name="mseller_ecf",
-    version=version,
+    version="0.1.0",
     description="Frappe/ERPNext integration for MSeller e-CF electronic invoicing",
     author="MSeller ECF Integration",
     author_email="support@example.com",
